873db9746de078dc3417f5c9d7a65e7ed9b20dea
Vu Van Dinh <anhdinh9596@gmail.com>
2023-09-15 10:01:26 +0700

    clean ordures

--- hotel-app/src/reducers/ProvincesReducer.ts
+++ hotel-app/src/reducers/ProvincesReducer.ts
@@ -17,10 +17,8 @@ const INITIAL_STATE = {
 };
 
 const provincesReducer = (state = INITIAL_STATE, action: IAction) => {
-  // console.log(action.payload);
   switch (action.type) {
     case actionTypes.GET_PROVINCE_REQUEST:
-      // console.log("FETCH_USERS_REQUEST:", action);
       return {
         ...state,
         isLoading: true,
@@ -28,8 +26,6 @@ const provincesReducer = (state = INITIAL_STATE, action: IAction) => {
       };
 
     case actionTypes.GET_PROVINCE_SUCCESS:
-      // console.log("FETCH_USERS_SUCCESS:", action);
-
       return {
         ...state,
         listProvinces: action.payload,
@@ -38,7 +34,6 @@ const provincesReducer = (state = INITIAL_STATE, action: IAction) => {
       };
 
     case actionTypes.GET_PROVINCE_ERROR:
-      // console.log("FETCH_USERS_ERROR:", action);
       return {
         ...state,
         isLoading: false,
@@ -46,7 +41,6 @@ const provincesReducer = (state = INITIAL_STATE, action: IAction) => {
       };
 
     case actionTypes.WATCH_FORM_VALUE:
-      console.log("Tao đã Set lại value:", action);
       localStorage.setItem(
         "selectedProvince",
         JSON.stringify({ ...action.payload })
